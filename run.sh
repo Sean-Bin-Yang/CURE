7e641d9e2fac7a58bf8373950092a7a7ddee7b6e
Sean Bin Yang <60964780+Sean-Bin-Yang@users.noreply.github.com>
2026-08-09 07:25:29 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -20,7 +20,7 @@ TASKS=("checkIn" "crime" "serviceCall")
 CITIES=("NY" "Chi" "SF")
 
 # lambda_causal search values
-LAMBDA_CAUSALS=("0" "0.01" "0.03" "0.05" "0.07" "0.09" "0.1" "0.3" "0.5" "0.7" "0.9")
+LAMBDA_CAUSALS=("0")
 
 for CITY in "${CITIES[@]}"; do
 
